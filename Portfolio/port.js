45b29a0dc878afc05f8f4770c5dd92414be56cff
BhavyaDBhardwaj <121045595+BhavyaDBhardwaj@users.noreply.github.com>
2023-08-15 22:25:00 +0530

    Update port.js

--- Portfolio/port.js
+++ Portfolio/port.js
@@ -2,16 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const downloadButton = document.getElementById('resume');
 
     downloadButton.addEventListener('click', function() {
-        // Replace 'example.pdf' with the actual path to your PDF file
         const pdfPath = 'Portfolio/CV/CV_bhavya.pdf';
 
-        // Create an anchor element
+        
         const anchor = document.createElement('a');
         anchor.href = pdfPath;
-        anchor.download = 'downloaded_CV.pdf'; // Set the desired file name for download
-        anchor.target = '_blank'; // Open the link in a new tab (optional)
-
-        // Simulate a click on the anchor element
+        anchor.download = 'downloaded_CV.pdf'; 
+        anchor.target = '_blank'; 
         anchor.click();
     });
 });
